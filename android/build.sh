039a3c699a67a71d067183de74a135d4b85b4f74
soralis0912 <soralis.nem@gmail.com>
2024-12-22 20:21:05 +0900

    Update build.sh

--- android/build.sh
+++ android/build.sh
@@ -10,7 +10,7 @@ export BUILD_NO=
 unset BUILD_NUMBER
 
 #TODO(zif): convert this to a runtime check, grep "sse4_2.*popcnt" /proc/cpuinfo
-#export CPU_SSE42=false
+export CPU_SSE42=false
 # Following env is set from build
 # VERSION
 # DEVICE
