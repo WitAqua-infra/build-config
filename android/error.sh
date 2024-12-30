d1ff93bc20d54960cf797c439027c49d4457152d
Syuugo <pub@s1204.me>
2024-12-30 16:13:52 +0900

    Update error.sh

--- android/error.sh
+++ android/error.sh
@@ -2,35 +2,40 @@
 
 set -eu
 
+if [ -z "$BUILD_UUID" ]; then
+  #export BUILD_UUID="$(uuidgen)"
+  export BUILD_UUID="$BUILDKITE_BUILD_ID"
+fi
+
 echo "--- Uploading logs on error"
-echo "failures/${DEVICE}/${BUILDKITE_BUILD_ID}/"
+echo "failures/${DEVICE}/${BUILD_UUID}/"
 
 if [ -f "/ssd02/WitAqua/setup/discord.sh" ]; then
   source /ssd02/WitAqua/setup/discord.sh
 fi
 
-if [ -f "/tmp/android-build-${BUILDKITE_BUILD_ID}.log" ]; then
+if [ -f "/tmp/android-build-${BUILD_UUID}.log" ]; then
   curl \
     -F content="## Build failed
-- UUID: \`${BUILDKITE_BUILD_ID}\`
-Please check **attached build log** and [**Buildkite**]($BUILDKITE_BUILD_URL)." \
-    -F "file1=@/tmp/android-build-${BUILDKITE_BUILD_ID}.log" \
-    -F "file2=@/tmp/android-sync-${BUILDKITE_BUILD_ID}.log" \
+- UUID: \`${BUILD_UUID}\`
+Please check **attached build log** and [**Buildkite**]($BUILD_UUID)." \
+    -F "file1=@/tmp/android-build-${BUILD_UUID}.log" \
+    -F "file2=@/tmp/android-sync-${BUILD_UUID}.log" \
     "$WEBHOOK_URL"
-elif [ -f "/tmp/android-sync-${BUILDKITE_BUILD_ID}.log" ]; then
+elif [ -f "/tmp/android-sync-${BUILD_UUID}.log" ]; then
   curl \
     -F content="## Sync failed
-- UUID: \`${BUILDKITE_BUILD_ID}\`
+- UUID: \`${BUILD_UUID}\`
 Please check **attached sync log** and [**Buildkite**]($BUILDKITE_BUILD_URL)." \
-    -F "file=@/tmp/android-sync-${BUILDKITE_BUILD_ID}.log" \
+    -F "file=@/tmp/android-sync-${BUILD_UUID}.log" \
     "$WEBHOOK_URL"
 else
   curl \
     -X POST \
     -H "Content-Type: application/json" \
-    -d "{\"content\": \"## Build failed\n- UUID: \`${BUILDKITE_BUILD_ID}\`\nLog not found.\nPlease check [**Buildkite**]($BUILDKITE_BUILD_URL)\"}" \
+    -d "{\"content\": \"## Build failed\n- UUID: \`${BUILD_UUID}\`\nLog not found.\nPlease check [**Buildkite**]($BUILDKITE_BUILD_URL)\"}" \
     "$WEBHOOK_URL"
 fi
 # ssh jenkins@blob.lineageos.org mkdir -p ~/incoming/failures/${DEVICE}/${BUILDKITE_BUILD_ID}/
-# scp /tmp/android-sync.log jenkins@blob.lineageos.org:incoming/failures/${DEVICE}/${BUILDKITE_BUILD_ID}/
-# scp /tmp/android-build.log jenkins@blob.lineageos.org:incoming/failures/${DEVICE}/${BUILDKITE_BUILD_ID}/
+# scp /tmp/android-sync-${BUILD_UUID}.log jenkins@blob.lineageos.org:incoming/failures/${DEVICE}/${BUILDKITE_BUILD_ID}/
+# scp /tmp/android-build-${BUILD_UUID}.log jenkins@blob.lineageos.org:incoming/failures/${DEVICE}/${BUILDKITE_BUILD_ID}/
